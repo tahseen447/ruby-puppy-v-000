0b133c9d3d973a3d52e2f86718a5ecdd37320bd2
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-06 23:38:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Dog
   @@all = []
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @@all << self
@@ -19,18 +19,14 @@ class Dog
 
   def self.all
     @@all.each {|item| puts "#{item}"}
->>>>>>> f07102d3b2207c07fd614863d165d7cf06f3b195
   end
 
   def self.clear_all
     @@all= []
   end
 
-<<<<<<< HEAD
-=======
   def name
     @name
   end
 
->>>>>>> f07102d3b2207c07fd614863d165d7cf06f3b195
 end
